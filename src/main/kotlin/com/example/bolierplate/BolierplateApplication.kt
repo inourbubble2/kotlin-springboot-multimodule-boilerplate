ec1bd8bd8d32d86a4c6d36881e00d244c1c5e4b8
Nayeon Kim <inourbubble2@gmail.com>
2023-11-12 22:22:28 +0900

    initial commit

---
+++ src/main/kotlin/com/example/bolierplate/BolierplateApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bolierplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BolierplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<BolierplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bolierplate/KotlinSpringbootMultimoduleBolierplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bolierplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootMultimoduleBolierplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
